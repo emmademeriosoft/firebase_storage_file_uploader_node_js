fc3bfa69c626f0dcfcd6596992593fcb38a90da5
emmademeriosoft <emmad.emeriosoft@gmail.com>
2022-01-22 22:55:23 +0500

    allow all files

--- utils/multer.js
+++ utils/multer.js
@@ -6,10 +6,10 @@ module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);  
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".pdf" && ext !== ".docx") {
-      cb(new Error("File type is not supported"), false);
-      return;
-    }
+    // if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".pdf" && ext !== ".docx") {
+    //   cb(new Error("File type is not supported"), false);
+    //   return;
+    // }
     cb(null, true);
   },
 });
