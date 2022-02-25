f6ad8f3ef2c811a9a0a9a75e65d71b1a2dd584f5
emmademeriosoft <emmad.emeriosoft@gmail.com>
2022-02-26 01:26:16 +0500

    updated

--- api/postAssignment.js
+++ api/postAssignment.js
@@ -87,7 +87,7 @@ router.get('/', async (req, res) => {
                 'user_name': user_name
             }
             const options = {
-                uri: 'http://localhost:80/emeriosoft-development/process/excludes/curl/set.php',
+                uri: 'https://www.wixtorstudio.com/emeriosoft-development/process/excludes/curl/set.php',
                 method: 'POST',
                 formData: formData,
                 headers: { 'user-agent': 'node.js' }
