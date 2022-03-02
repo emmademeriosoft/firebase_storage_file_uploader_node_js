f740f171e12c6899c9bde805f779b2f1b231f110
emmademeriosoft <emmad.emeriosoft@gmail.com>
2022-03-02 20:41:42 +0500

    changes

--- api/postAssignment.js
+++ api/postAssignment.js
@@ -85,7 +85,7 @@ router.get('/', async (req, res) => {
                 'user_name': user_name
             }
             const options = {
-                uri: req.get('origin')+'/process/excludes/curl/set.php',
+                uri: req.get('origin')+'/portal/process/excludes/curl/set.php',
                 method: 'POST',
                 formData: formData,
                 headers: { 'user-agent': 'node.js' }
