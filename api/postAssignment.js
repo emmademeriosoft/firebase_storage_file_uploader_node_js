6d4909fa444576c90fcb7f989cb29cbb3d21fefc
emmademeriosoft <emmad.emeriosoft@gmail.com>
2022-04-23 02:36:23 +0500

    new route for student area

--- api/postAssignment.js
+++ api/postAssignment.js
@@ -90,6 +90,8 @@ router.get('/', async (req, res) => {
                 formData: formData,
                 headers: { 'user-agent': 'node.js' }
             }
+            console.log(options)
+            console.log(req)
             request(options, (error, response, body) => {
                 if (error) {
                     console.error(error.message);
@@ -147,6 +149,134 @@ router.post('/download_all', async (req, res) => {
                         formData: formData,
                         headers: { 'user-agent': 'node.js' }
                     }
+                    console.log(options)
+                    console.log(req)
+                    request(options, (error, response, body) => {
+                        if (error) {
+                            console.error(error.message);
+                            return res.status(400).json({ errors: error })
+                        }
+                        if (response.statusCode !== 200) {
+                            return res.status(404).json({ msg: 'error in request' })
+                        }
+                        res.json(JSON.parse(body));
+                    })
+                }
+            }).catch((error) => {
+                return res.status(500).json();
+            });
+        })
+
+    } catch (err) {
+        console.log(err);
+    }
+
+})
+
+
+
+
+
+
+// student area 
+
+
+
+router.get('/student_area', async (req, res) => {
+    try {
+        let destFilename = './Downloads';
+        const options = {
+            // The path to which the file should be downloaded
+            destination: destFilename,
+        };
+        const urlOptions = {
+            version: "v4",
+            action: "read",
+            expires: Date.now() + 1000 * 60 * 2, // 2 minutes
+        }
+        // res.setHeader('Access-Control-Allow-Origin', '*');
+        // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        // res.setHeader('Access-Control-Allow-Credentials', true);
+
+
+        storageRef.file("assignments/" + req.query.fileName).getSignedUrl(urlOptions).then((value) => {
+            file_name = req.query.fileName
+            user_name = req.query.user
+            const [exclude, ...rest] = file_name.split('-')
+            file_name = rest.join('-')
+            const formData = {
+                'file_url': value,
+                'fileName': file_name,
+                'user_name': user_name
+            }
+            const options = {
+                uri: req.get('origin') + '/area1/api/transfer.php',
+                method: 'POST',
+                formData: formData,
+                headers: { 'user-agent': 'node.js' }
+            }
+            console.log(options)
+            console.log(req)
+            request(options, (error, response, body) => {
+                if (error) {
+                    console.error(error.message);
+                    return res.status(400).json({ errors: error })
+                }
+                if (response.statusCode !== 200) {
+                    return res.status(404).json({ msg: 'error in request' })
+                }
+                res.json(JSON.parse(body));
+            })
+        }).catch((error) => {
+            console.log(error);
+            return res.status(500).json();
+        });
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+router.post('/student_area/download_all', async (req, res) => {
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+    try {
+        let files_detail = [];
+        let user_name = req.body.user
+        let order_title = req.body.order_title
+        const urlOptions = {
+            version: "v4",
+            action: "read",
+            expires: Date.now() + 1000 * 60 * 2, // 2 minutes
+        }
+        let totalCountFiles = req.body.download_files.length;
+        await req.body.download_files.forEach((fileName, key) => {
+
+            storageRef.file("assignments/" + fileName).getSignedUrl(urlOptions).then((value) => {
+                file_name = fileName
+                const [exclude, ...rest] = file_name.split('-')
+                file_name = rest.join('-')
+                files_detail.push({
+                    url: value,
+                    file_name
+                })
+                totalCountFiles--;
+                if (totalCountFiles == 0) {
+                    const formData = {
+                        'files_detail': JSON.stringify(files_detail),
+                        'user_name': user_name,
+                        'order_title': order_title
+                    }
+                    const options = {
+                        uri: req.get('origin') + '/area1/api/transfer.php',
+                        method: 'POST',
+                        formData: formData,
+                        headers: { 'user-agent': 'node.js' }
+                    }
+                    console.log(options)
+                    console.log(req)
                     request(options, (error, response, body) => {
                         if (error) {
                             console.error(error.message);
